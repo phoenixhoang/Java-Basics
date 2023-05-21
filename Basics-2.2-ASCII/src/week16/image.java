273f9931eba6a9913fe2c4a5aded39f9419c561b
Phoenix Hoang <80337667+phoenixhoang@users.noreply.github.com>
2023-05-21 22:17:21 +0100

    ASCII

---
+++ Basics-2.2-ASCII/src/week16/image.java
@@ -0,0 +1,12 @@
+package week16;
+
+public class image {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Image2Ascii img = new Image2Ascii();
+		img.LoadImage("cat.png");
+		img.Print();
+	}
+
+}
